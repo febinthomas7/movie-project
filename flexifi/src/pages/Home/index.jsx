476aff56e2215ca8cc6a4a1148cc88caf7060d4c
febin thomas <tfebin39@gmail.com>
2024-02-21 12:06:04 +0530

    2nd commit

---
+++ flexifi/src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-black text-white">
+      <h1>welcome to home page</h1>
+    </div>
+  );
+};
+
+export default Home;
